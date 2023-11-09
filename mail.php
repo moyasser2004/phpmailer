ecc5e452dd55e8b01bf46425c136c693aae0f7ff
moyasser2004 <124683941+moyasser2004@users.noreply.github.com>
2023-11-09 15:59:16 +0200

    modified:   mail.php

--- mail.php
+++ mail.php
@@ -11,12 +11,12 @@ require 'mailer/autoload.php';
 $mail = new PHPMailer();
 
     //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.example.com';                                 //Set the SMTP server to send through
+    $mail->Host       = 'smtp.gmail.com';                                 //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'user@example.com';                           //SMTP username
-    $mail->Password   = 'secret';                        //SMTP password
+    $mail->Username   = 'mohamedyasser.alkotp@gmail.com';                           //SMTP username
+    $mail->Password   = 'pwic nmis pumx oqro';                        //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
     
